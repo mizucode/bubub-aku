4344bf221ab3ab5556755449a5a1b96f3cabf79f
mizucode <mizurisysthensis3@gmail.com>
2023-03-22 09:33:32 +0700

    fix navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,15 @@ module.exports = {
                 dsr: "#e384ff",
                 ylw: "#f4bc2b",
                 dark: "#232323",
+                darkdsr: "#37306B",
+                lightdsr: "#66347F",
             },
             screens: {
                 "2xl": "1320px",
             },
             backgroundImage: {
                 "hero-pattern": "url('/img/hero.jpeg')",
+                "sidebar-pattern": "url('/img/sidebar.webp')",
             },
             fontSize: {
                 "10xl": "10rem",
